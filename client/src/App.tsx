a918e567942a2a6bb65f6dcce5e99a302fa353ba
OjasChatur <ojaschatur18@gmail.com>
2025-09-06 19:13:50 +0530

    Added Frontend using ReactJS

---
+++ client/src/App.tsx
@@ -0,0 +1,22 @@
+import { Container, Stack } from "@chakra-ui/react"
+import Navbar from "./components/ui/Navbar"
+import TodoForm from "./components/ui/TodoForm"
+import TodoList from "./components/ui/TodoList"
+
+
+export const BASE_URL = "http://localhost:5000/apis"
+function App() {
+
+
+  return (
+    <Stack h="100vh">
+      <Navbar/>
+      <Container>
+        <TodoForm/>
+        <TodoList/>
+      </Container>
+    </Stack>
+  )
+}
+
+export default App
---
+++ client/src/components/ui/TodoForm.tsx
@@ -0,0 +1,62 @@
+import { BASE_URL } from "../../App";
+import { Button, Flex, Input, Spinner } from "@chakra-ui/react";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useState } from "react";
+import { IoMdAdd } from "react-icons/io";
+
+const TodoForm = () => {
+	const [newTodo, setNewTodo] = useState("");
+    const queryClient = useQueryClient();
+
+    const {mutate:createTodo, isPending:isCreating} = useMutation( {
+        mutationKey: ['createTodo'],
+        mutationFn: async (e:React.FormEvent) => {
+            e.preventDefault();
+            try {
+                const res = await fetch(BASE_URL + '/todos', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({body: newTodo})
+                });
+                const data = await res.json();
+                if (!res.ok) {
+                    throw new Error(data.message || 'Failed to create todo');
+                }
+                setNewTodo('');
+                return data;
+            } catch (error) {
+                console.error(error);
+            }
+        },
+        onSuccess: () => {
+            queryClient.invalidateQueries({ queryKey: ['todos'] });
+        },
+        onError: (error) => {
+            console.error('Error creating todo:', error);
+        }
+    })
+	return (
+		<form onSubmit={createTodo}>
+			<Flex gap={2}>
+                <Input
+                type='text'
+                value={newTodo}
+                onChange={(e) => setNewTodo(e.target.value)}
+                autoFocus
+                />
+				<Button
+					mx={2}
+					type='submit'
+					_active={{
+						transform: "scale(.97)",
+					}}
+				>
+					{isCreating ? <Spinner size={"xs"} /> : <IoMdAdd size={30} />}
+				</Button>
+			</Flex>
+		</form>
+	);
+};
+export default TodoForm;
